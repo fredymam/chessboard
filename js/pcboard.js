0da1a8f48d27bc9a8f32f98821acab631a237c05
Marcelo <marcelomana@educ.ar>
2022-02-15 14:50:09 -0300

    re-estructuracion de archivos

---
+++ js/pcboard.js
@@ -0,0 +1,28 @@
+var board;
+var jugada = 0;
+
+function onDrop (source, target, piece, newPos, oldPos, orientation) {
+    jugada++;
+    const movimiento = document.createTextNode(jugada + '. ' + piece + ' ' +  (source === 'spare' ? '-' : source) + ' ' + target); 
+    const parrafo = document.createElement("li");
+    parrafo.appendChild(movimiento);
+    document.getElementById("notacion").appendChild(parrafo); 
+}
+
+function init() {
+	board = Chessboard('tablero', {
+                draggable: true,
+                dropOffBoard: 'trash',
+                sparePieces: true,
+                showNotation: true,
+                onDrop: onDrop});			
+	$(window).resize(board.resize);				
+	$('#startBtn').on('click', board.start);
+	$('#flipBtn').on('click', board.flip)
+	$('#clearBtn').on('click', board.clear);
+	$('#quiz1Btn').on('click', function () { board.position({a1: 'wK', e4: 'wQ'}) });
+	$('#quiz2Btn').on('click', function () { board.position('8/8/8/8/PPP5/NNBP4/RBNP4/KRNP4') });
+	$('#quiz3Btn').on('click', function () { board.position('2r5/1p1n4/P1pbq3/1P1p1k2/2P1pbn1/3P1p1r/4P1p1/N4P2') });
+	$('#quiz4Btn').on('click', function () { board.position('8/8/8/8/PP6/8/2P5/rnb5') });
+}
+
