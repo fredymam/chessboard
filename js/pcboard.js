5f36f6e8a945dc1048dfd2c3199f9b008cc97863
Marcelo <marcelomana@educ.ar>
2022-02-16 12:27:52 -0300

    bug array

--- js/pcboard.js
+++ js/pcboard.js
@@ -1,10 +1,9 @@
 var tablero;
 var jugada = 0;
-var desafios = ["''",
-	 			"'8/8/8/8/4Q3/8/8/K7'",
-				"'8/8/8/8/PPP5/NNBP4/RBNP4/KRNP4'",
-				"'2r5/1p1n4/P1pbq3/1P1p1k2/2P1pbn1/3P1p1r/4P1p1/N4P2'",
-				"'8/8/8/8/PP6/8/2P5/rnb5'"];
+var desafios = ['8/8/8/8/4Q3/8/8/K7',
+				'8/8/8/8/PPP5/NNBP4/RBNP4/KRNP4',
+				'2r5/1p1n4/P1pbq3/1P1p1k2/2P1pbn1/3P1p1r/4P1p1/N4P2',
+				'8/8/8/8/PP6/8/2P5/rnb5'];
 
 function addJugada (source, target, piece, newPos, oldPos, orientation) {
 	if (!(source == 'spare' || target == 'offboard')) {
@@ -33,8 +32,8 @@ function dropMovimientos () {
 
 function loadDesafio (num) {
   dropMovimientos();
-  alert(desafios[num]);
-  tablero.position(desafios[num]);	
+  if (num==0) tablero.clear()
+  else tablero.position(desafios[num]);	
 }
 
 function inicializar() {
@@ -46,7 +45,7 @@ function inicializar() {
                 onDrop: addJugada});			
 	$(window).resize(tablero.resize);				
 	$('#flipBtn').on('click', tablero.flip);
-	$('#clearBtn').on('click', function () { tablero.postion('') });
+	$('#clearBtn').on('click', function () { loadDesafio(0) });
 	$('#quiz1Btn').on('click', function () { loadDesafio(1) });;
 	$('#quiz2Btn').on('click', function () { loadDesafio(2) });
 	$('#quiz3Btn').on('click', function () { loadDesafio(3) });
