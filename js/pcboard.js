5f095f44244f6cee3d246c28a1fa19a95e5a5a19
Marcelo <marcelomana@educ.ar>
2022-02-16 13:21:44 -0300

    verticalalign

--- js/pcboard.js
+++ js/pcboard.js
@@ -15,7 +15,7 @@ function addJugada (source, target, piece, newPos, oldPos, orientation) {
 	    trebejo.src = "img/chesspieces/wikipedia/"+piece+".png";
 	    trebejo.style.width = "2em";
 	    trebejo.style.height = "2em";
-	    trebejo.style.align = "top";
+	    trebejo.style.verticalalign = "top";
 	    parrafo.appendChild(trebejo);
 	    var coordenada = document.createTextNode(' '+source +'-'+ target);
         parrafo.appendChild(coordenada);
