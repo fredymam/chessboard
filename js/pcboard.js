521ee7b375ec70a8288d6e774cc4a6650f82acdb
Marcelo <marcelomana@educ.ar>
2022-02-16 09:45:27 -0300

    debug

--- js/pcboard.js
+++ js/pcboard.js
@@ -7,7 +7,7 @@ function onDrop (source, target, piece, newPos, oldPos, orientation) {
 	    const movimiento = document.createTextNode(jugada + '. ' + piece + ' ' +  source + '-' + target); 
 	    const parrafo = document.createElement("p");
 	    parrafo.appendChild(movimiento);
-	    document.getElementById("notacion").appendChild(parrafo);		
+	    document.getElementById("notacion").appendChild(parrafo);	
 	}
 }
 
@@ -29,7 +29,7 @@ function inicializar() {
                 onDrop: onDrop});			
 	$(window).resize(tablero.resize);				
 	$('#flipBtn').on('click', tablero.flip);
-	$('#clearBtn').on('click', dropMovimientos);
+	$('#clearBtn').on('click', tablero.clear);
 	$('#quiz1Btn').on('click', function () { tablero.position({a1: 'wK', e4: 'wQ'}) });
 	$('#quiz2Btn').on('click', function () { tablero.position('8/8/8/8/PPP5/NNBP4/RBNP4/KRNP4') });
 	$('#quiz3Btn').on('click', function () { tablero.position('2r5/1p1n4/P1pbq3/1P1p1k2/2P1pbn1/3P1p1r/4P1p1/N4P2') });
