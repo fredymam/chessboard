040cfb3a5109dead6958f53a603cfdec75a40b23
Marcelo <marcelomana@educ.ar>
2022-02-22 16:16:30 -0300

    Se agrega Enunciados

--- js/pcboard.js
+++ js/pcboard.js
@@ -1,10 +1,6 @@
 var tablero;
 var jugada = 0;
-var desafios = ['1pppppp1/pppppppp/pppppppp/pppppppp/ppppQppp/pppppppp/pppppppp/Kpppppp1',
-				'8/8/8/8/PPP5/NNBP4/RBNP4/KRNP4',
-				'2r5/1p1n4/P1pbq3/1P1p1k2/2P1pbn1/3P1p1r/4P1p1/N4P2',
-				'8/8/8/8/PP6/8/2P5/rnb5'];
-
+				
 function addJugada (source, target, piece, newPos, oldPos, orientation) {
 	if (!(source == 'spare' || target == 'offboard') && (source != target)) {
 		var parrafo = document.createElement("p");
@@ -16,10 +12,9 @@ function addJugada (source, target, piece, newPos, oldPos, orientation) {
 	    trebejo.style.height = "1.5em";
 	    trebejo.style.verticalAlign = "text-bottom";
 	    parrafo.appendChild(trebejo);
-/* 	    var coordenada = document.createTextNode(target + ' (' + source + '-' + target + ')'); */
 	    var coordenada = document.createTextNode(source + '-' + target);
-        parrafo.appendChild(coordenada);
-		document.getElementById("movimientos").appendChild(parrafo);	
+	    parrafo.appendChild(coordenada);
+	    document.getElementById("movimientos").appendChild(parrafo);	
 	}
 }
 
@@ -33,8 +28,13 @@ function dropMovimientos () {
 
 function loadDesafio (num) {
   dropMovimientos();
-  if (num==0) tablero.clear()
-  else tablero.position(desafios[--num]);	
+  if (num==0) {
+	tablero.clear();
+	document.getElementById("consigna").innerHTML = "";
+  } else {
+  	tablero.position(desafio[--num].posicion);
+    document.getElementById("consigna").innerHTML = "<h3>"+desafio[num].titulo +"</h3>" + desafio[num].consigna;
+  }
 }
 
 function inicializar() {
@@ -50,5 +50,5 @@ function inicializar() {
 	$('#quiz1Btn').on('click', function () { loadDesafio(1) });
 	$('#quiz2Btn').on('click', function () { loadDesafio(2) });
 	$('#quiz3Btn').on('click', function () { loadDesafio(3) });
-	$('#quiz4Btn').on('click', function () { loadDesafio(4) }); 
+	$('#quiz4Btn').on('click', function () { loadDesafio(4) });
 }
